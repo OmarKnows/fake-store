bebeed8e7469155a463794f712a24b092f0bb3fb
OmarKnows <omarknows98@gmail.com>
2023-11-08 13:31:56 +0200

    redesigned product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 import { RootState } from '@/redux/store';
 import { useEffect } from 'react';
 import ProductRating from '@/components/ProductRating';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 const ProductPage = () => {
 	const dispatch = useAppDispatch();
@@ -12,6 +14,7 @@ const ProductPage = () => {
 
 	useEffect(() => {
 		id && dispatch(getProduct(id));
+		console.log(product);
 	}, [dispatch]);
 
 	return (
@@ -19,16 +22,28 @@ const ProductPage = () => {
 			{loading && <p>Loading...</p>}
 			{error && <p>Error: {error}</p>}
 			{!loading && !error ? (
-				<div>
-					<div className='flex justify-center items-center divide-x-2 p-3'>
-						<div>
-							<h1 className='font-bold text-3xl p-3'>{product?.title}</h1>
-							<p className='w-1/2'>{product?.description}</p>
-							<ProductRating value={product?.rating} />
-							<p>Price: ${product?.price}</p>
+				<div className='flex justify-center mt-[15vh] '>
+					<div className='flex border border-1 border-gray-300'>
+						<div className='w-60 border border-1 border-gray-300 p-5'>
+							<img className='w-100 h-100 object-contain' src={product?.image} />
 						</div>
-						<div>
-							<img src={product?.image} />
+						<div className='text-center px-5'>
+							<p className='text-3xl font-bold mt-5'>{product?.title}</p>
+							<p className='my-10'>Description: {product?.description}</p>
+							<div className='flex justify-between my-10'>
+								<div className='text-2xl items-center'>${product?.price}</div>
+								<div className='text-2xl'>
+									<ProductRating value={product?.rating} />
+								</div>
+							</div>
+							<div className='flex justify-between my-10'>
+								<div className='text-2xl items-center'>
+									<Input type='number' />
+								</div>
+								<div className='text-2xl'>
+									<Button>Add To Cart</Button>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
