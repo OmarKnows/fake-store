8a2b01c8b204eb193a2ed6cee33ba6bcef79f2cf
OmarKnows <omarknows98@gmail.com>
2023-11-08 10:45:42 +0200

    adjusted design of products page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import { IProduct } from '@/redux/products/productsModel';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '@/components/ui/card';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProductRating from './ProductRating';
 
 interface IProductCardProps {
 	product: IProduct;
@@ -9,26 +10,25 @@ interface IProductCardProps {
 
 const ProductCard: React.FC<IProductCardProps> = ({ product }) => {
 	return (
-		<div key={product.id}>
-			<Link to={`${product.id}`}>
-				<Card>
-					<CardContent>
-						<div className='h-64 flex justify-center'>
-							<img className='h-full' src={product.image} />
-						</div>
-					</CardContent>
-					<CardHeader>
-						<h2 className='truncate'>{product?.title}</h2>
-					</CardHeader>
-					<CardDescription>
-						<p className='truncate'>Description: {product?.description}</p>
-					</CardDescription>
-					<CardFooter>
-						<p>Price: ${product?.price}</p>
-					</CardFooter>
-				</Card>
-			</Link>
-		</div>
+		<Link to={`${product.id}`}>
+			<Card>
+				<CardContent>
+					<div className='h-64 flex justify-center'>
+						<img className='h-full' src={product.image} />
+					</div>
+				</CardContent>
+				<CardHeader>
+					<h2 className='truncate'>{product?.title}</h2>
+				</CardHeader>
+				<CardDescription>
+					<p className='truncate'>{product?.description}</p>
+				</CardDescription>
+				<CardFooter className='flex justify-between'>
+					<ProductRating value={product.rating} />
+					<p>Price: ${product?.price}</p>
+				</CardFooter>
+			</Card>
+		</Link>
 	);
 };
 
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -10,16 +10,20 @@ const ProductsPage = () => {
 
 	useEffect(() => {
 		dispatch(getProducts());
+		console.log(products);
 	}, [dispatch]);
 
 	return (
 		<div>
 			{loading && <p>Loading...</p>}
 			{error && <p>Error: {error}</p>}
+			<h1 className='font-bold text-3xl p-3'>Products</h1>
 			{!loading && !error ? (
 				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-3	'>
 					{products?.map((product) => (
-						<ProductCard product={product} />
+						<div key={product.id}>
+							<ProductCard product={product} />
+						</div>
 					))}
 				</div>
 			) : (
