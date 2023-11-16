62fc19eda4c00c9666e56a5d4204150ee5352810
OmarKnows <omarknows98@gmail.com>
2023-11-16 11:00:03 +0200

    Footer Design

---
+++ src/components/Footer.tsx
@@ -0,0 +1,52 @@
+import fb from '@/assets/facebook.png';
+import tw from '@/assets/twitter.png';
+
+const Footer = () => {
+	return (
+		<div className='flex justify-between py-10 px-12 bg-gray-800 mt-24 text-2xl'>
+			<div>
+				<div className='text-white font-light'>Fake Store</div>
+				<ul className='font-medium text-gray-600 ps-8 mt-5 list-disc'>
+					<li>About Us</li>
+					<li>Contact Us</li>
+					<li>Our Store</li>
+					<li>Blog</li>
+					<li>Site Map</li>
+				</ul>
+			</div>
+			<div>
+				<div className='text-white font-light'>Customer Info</div>
+				<ul className='font-medium text-gray-600 ps-8 mt-5 list-disc'>
+					<li>Delivery</li>
+					<li>Returns</li>
+					<li>Cookie Policy</li>
+					<li>Terms & Conditions</li>
+					<li>Privacy Policy</li>
+				</ul>
+			</div>
+			<div>
+				<div className='text-white font-light'>Features</div>
+				<ul className='font-medium text-gray-600 ps-8 mt-5 list-disc'>
+					<li>Best Sellers</li>
+					<li>Shop The Sale</li>
+					<li>Gift Vouchers</li>
+					<li>Meet The Team</li>
+					<li>Competitions</li>
+				</ul>
+			</div>
+			<div>
+				<div className='text-white font-light'>Follow Us</div>
+				<div className='flex gap-8 mt-4 ps-4'>
+					<div className='h-7 w-7'>
+						<img className='w-full h-full object-contain' src={fb} />
+					</div>
+					<div className='h-7 w-7'>
+						<img className='w-full h-full object-contain' src={tw} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,9 +1,9 @@
 import NavBar from '@/components/NavBar';
-import HomePage from '@/pages/HomePage';
 import LoginPage from '@/pages/LoginPage';
 import ProductPage from '@/pages/ProductPage';
 import ProductsPage from '@/pages/ProductsPage';
 import { Outlet, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Footer from './Footer';
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -13,13 +13,13 @@ export const router = createBrowserRouter(
 				<>
 					<NavBar />
 					<Outlet />
+					<Footer />
 				</>
 			}
 		>
 			<Route index path='/' element={<LoginPage />} />
 			<Route path='/products' element={<ProductsPage />} />
 			<Route path='/products/:id' element={<ProductPage />} />
-			<Route path='/home' element={<HomePage />} />
 		</Route>
 	)
 );
